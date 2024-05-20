47382de47cc1a433db2fdd096f1e5639cb60d0c0
Luis Henrique <duimbispo@gmail.com>
2024-05-20 13:15:10 -0300

    Criação da página Sobre Nos

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('verMaisBtn').addEventListener('click', function() {
+        var extraText = document.querySelector('.extra-text');
+        var ellipsis = document.getElementById('ellipsis');
+        
+        if (extraText.style.display === 'none' || extraText.style.display === '') {
+            extraText.style.display = 'inline';
+            ellipsis.style.display = 'none'; // Esconde o "..."
+            this.style.display = 'none'; // Esconde o botão
+        }
+    });
+});
